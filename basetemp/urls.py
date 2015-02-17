11cea682d2fa5d7b43f36cc58fd4f40a1031bcd8
tselland <tselland@byu.edu>
2015-02-16 19:47:34 -0500

    Friday Forecast changes to set up for outputting forecast messages from form to html

--- basetemp/urls.py
+++ basetemp/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'project1.views.index'),
+    url(r'^friday', 'friday.views.index'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
