ff5c240019c9678060a7bc46ec1922fa603fee4a
tselland <tselland@byu.edu>
2015-02-24 23:27:07 -0700

    Date Picker Widget added to form

--- lib/widgets.py
+++ lib/widgets.py
@@ -1,24 +1,52 @@
 __author__ = 'travisselland'
 
 
-class DateWidget(PickerWidgetMixin, DateInput):
+import django.forms
+import itertools
+
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
+from django.forms.util import flatatt
+from django.utils.encoding import force_text
+
+class DatePickerWidget(django.forms.TextInput):
     """
-    DateWidget is the corresponding widget for Date field, it renders only the date section of
-    datetime picker.
+    Custom date picker widget to use with TextInput fields
     """
 
-    format_name = 'DATE_INPUT_FORMATS'
-    glyphicon = 'glyphicon-calendar'
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    def __init__(self, attrs=None, options=None, usel10n=None, bootstrap_version=None):
+    def render(self, name, value='', attrs={}, choices=()):
 
-        if options is None:
-            options = {}
+        attrs['data-toggle'] = 'buttons'
+        attrs.update(self.div_attrs)
+        final_attrs = self.build_attrs(attrs)
+        output = []
+        output.append(format_html('<div{0}>', flatatt(final_attrs)))
+        options = self.render_options(attrs['id'], name, choices, [value])
+        if options:
+            output.append(options)
+        output.append('</div>')
 
-        # Set the default options to show only the datepicker object
-        options['startView'] = options.get('startView', 2)
-        options['minView'] = options.get('minView', 2)
-        options['format'] = options.get('format', 'dd/mm/yyyy')
+        return mark_safe('\n'.join(output))
 
-        super(DateWidget, self).__init__(attrs, options, usel10n, bootstrap_version)
+    def render_options(self, elem_id, name, choices, selected_choices):
 
+        selected_choices = set(force_text(v) for v in selected_choices)
+        output = []
+        for i, (option_value, option_label) in enumerate(itertools.chain(self.choices, choices)):
+            option_value = force_text(option_value)
+            btn_class = mark_safe(self.btn_class)
+            checked = ''
+            if option_value in selected_choices:
+                btn_class = mark_safe('%s %s' % (btn_class, 'active'))
+                checked = 'checked'
+            output.append(format_html('<label class="{0}"><input type="radio" value="{1}" name="{2}" id="{3}" {4} autocomplete="off"/>{5}</label>',
+                             btn_class,
+                             option_value,
+                             name,
+                             '%s_%s' % (elem_id, i),
+                             checked,
+                             option_label))
+        return '\n'.join(output)
