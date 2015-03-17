de516d753328358798ef8373484e0f33592e12c2
tselland <tselland@byu.edu>
2015-03-16 20:09:44 -0600

    removed initial templater and implemented new one.

--- pagination/views.py
+++ pagination/views.py
@@ -2,15 +2,14 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 # Create your views here.
 from pagination import models as mod
-from lib import tables
+from lib.tables import Table
 
 
 def pagination(request):
     params = {}
 
     params['initial_page'] = 0
-
-    return render_to_response('table_demo.html', params)
+    return render_to_response('table_demo1.html', params)
 
 
 def get_table(request):
@@ -19,10 +18,10 @@ def get_table(request):
     users = UserTable(mod.User.objects.all())
     users.paginate(request)
 
-    params['users'] = users
+    params['table'] = users
 
     return render_to_response('tabledemo.get_table.html', params)
 
-class UserTable(tables.Table):
+class UserTable(Table):
     headers = ['First Name', 'Last Name', 'Email']
     fields = ['first_name', 'last_name', 'email']
